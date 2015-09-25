bfb02d5586a672c444ccc9986687babff788ad4d
Cameron Hunter <chunter@twitter.com>
2015-09-24 17:17:11 -0700

    Small change

--- templates/lib/skill.js
+++ templates/lib/skill.js
@@ -1,13 +1,13 @@
-export default class <%= _.camelize(name) %> {
+export default class <%= _.classify(name) %> {
   launch() {
-    console.log("<%= _.camelize(name) %> launched!");
+    console.log("<%= _.classify(name) %> launched!");
   }
 
   myIntent(slots) {
-    console.log(`Intent myIntent called on <%= _.camelize(name) %> with data "${JSON.stringify(slots)}"`);
+    console.log(`Intent myIntent called on <%= _.classify(name) %> with data "${JSON.stringify(slots)}"`);
   }
 
   intent(name, slots) {
-    console.log(`Intent "${name}" called on <%= _.camelize(name) %> with data "${JSON.stringify(slots)}"`);
+    console.log(`Intent "${name}" called on <%= _.classify(name) %> with data "${JSON.stringify(slots)}"`);
   }
 }
