f06400f0ebecf0149f53197721714d672f4f17d6
Cameron Hunter <chunter@twitter.com>
2015-09-27 13:18:58 -0700

    Update skill spec

--- generators/app/templates/test/skill.spec.js
+++ generators/app/templates/test/skill.spec.js
@@ -1,55 +1,16 @@
 import chai from "chai";
 import promised from "chai-as-promised";
+import { Response } from "alexa-lambda-skill";
 import <%= className %> from "../lib/<%= fileName %>";
 
 chai.should();
 chai.use(promised);
 
-const handler = (request) => Handler(request, {
-  succeed() {},
-  fail() {}
-});
-
 describe("<%= className %>", function() {
-  describe("LaunchRequest", function() {
+  describe("launch", function() {
     it("should say that it was launched", function() {
-      const result = handler({ request: { type: "LaunchRequest" } });
-      return result.should.eventually.deep.equal({
-        version: "1.0",
-        sessionAttributes: {},
-        response: {
-          shouldEndSession: true,
-          outputSpeech: { type: "PlainText", text: "Echo launched!" }
-        }
-      });
-    });
-  });
-
-  describe("IntentRequest", function() {
-    describe("hello intent", function() {
-      it("should say hello world", function() {
-        const result = Handler({ request: { type: "IntentRequest", intent: { name: "hello" } } }, context);
-        return result.should.eventually.deep.equal({
-          version: "1.0",
-          sessionAttributes: {},
-          response: {
-            shouldEndSession: true,
-            outputSpeech: { type: "PlainText", text: "Hello world" }
-          }
-        });
-      });
-
-      it("should say hello name when provided", function() {
-        const result = Handler({ request: { type: "IntentRequest", intent: { name: "hello", slots: { name: { name: "name", value: "echo" } } } } }, context);
-        return result.should.eventually.deep.equal({
-          version: "1.0",
-          sessionAttributes: {},
-          response: {
-            shouldEndSession: true,
-            outputSpeech: { type: "PlainText", text: "Hello world" }
-          }
-        });
-      });
+      const result = new <%= className %>().launch();
+      return result.should.eventually.be.an.instanceOf(Response);
     });
   });
 });
