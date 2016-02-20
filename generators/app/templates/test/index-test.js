7efd9238e211c94a2fc93fda0b05f68d3660082f
Cameron Hunter <chunter@twitter.com>
2016-02-19 16:59:11 -0800

    Update test template

--- generators/app/templates/test/index-test.js
+++ generators/app/templates/test/index-test.js
@@ -1,28 +1,35 @@
 import test from 'ava';
 import { Request } from 'alexa-lambda-skill';
-import <%= className %> from '..';
+import { handler } from '..';
+
+// AWS Lambda requires a CommonJS export named 'handler'
+const <%= className %> = handler.default;
 
 test('LaunchRequest', t => {
   const event = Request.launchRequest().build();
 
-  return t.same(<%= className %>(event), {
-    version: '1.0',
-    response: {
-      shouldEndSession: true,
-      outputSpeech: { type: 'PlainText', text: '<%= className %> launched!' }
-    }
+  return <%= className %>(event).then(response => {
+    t.same(response, {
+      version: '1.0',
+      response: {
+        shouldEndSession: true,
+        outputSpeech: { type: 'PlainText', text: '<%= className %> launched!' }
+      }
+    });
   });
 });
 
 test('Hello intent', t => {
   const event = Request.intent('hello', { name: 'world' }).build();
 
-  return t.same(<%= className %>(event), {
-    version: '1.0',
-    response: {
-      shouldEndSession: true,
-      outputSpeech: { type: 'PlainText', text: 'Hello world' },
-      card: { type: 'Simple', title: '<%= className %>', content: 'Hello world' }
-    }
+  return <%= className %>(event).then(response => {
+    t.same(response, {
+      version: '1.0',
+      response: {
+        shouldEndSession: true,
+        outputSpeech: { type: 'PlainText', text: 'Hello world' },
+        card: { type: 'Simple', title: '<%= className %>', content: 'Hello world' }
+      }
+    });
   });
 });
